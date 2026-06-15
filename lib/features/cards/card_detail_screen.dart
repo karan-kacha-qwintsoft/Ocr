2acadd1d55005be1b1046488fcb8950e3bf672a8
kachakaran6 <kachakaran06@gmail.com>
2026-06-15 17:48:17 +0530

    all fields map

--- lib/features/cards/card_detail_screen.dart
+++ lib/features/cards/card_detail_screen.dart
@@ -11,15 +11,30 @@ final cardDetailProvider = FutureProvider.family<Map<String, dynamic>, String>((
   final Map<String, dynamic> data = {};
   
   if (coreFields != null) {
-    if (coreFields.fullName != null && coreFields.fullName!.isNotEmpty) data['full_name'] = coreFields.fullName;
-    if (coreFields.phonePrimary != null && coreFields.phonePrimary!.isNotEmpty) data['phone_primary'] = coreFields.phonePrimary;
-    if (coreFields.phoneSecondary != null && coreFields.phoneSecondary!.isNotEmpty) data['phone_secondary'] = coreFields.phoneSecondary;
-    if (coreFields.emailPrimary != null && coreFields.emailPrimary!.isNotEmpty) data['email_primary'] = coreFields.emailPrimary;
-    if (coreFields.website != null && coreFields.website!.isNotEmpty) data['website'] = coreFields.website;
-    if (coreFields.aadhaarNumber != null && coreFields.aadhaarNumber!.isNotEmpty) data['aadhaar_number'] = coreFields.aadhaarNumber;
-    if (coreFields.dob != null && coreFields.dob!.isNotEmpty) data['dob'] = coreFields.dob;
-    if (coreFields.gender != null && coreFields.gender!.isNotEmpty) data['gender'] = coreFields.gender;
-    if (coreFields.notes != null && coreFields.notes!.isNotEmpty) data['notes'] = coreFields.notes;
+    if (coreFields.fullName != null && coreFields.fullName!.isNotEmpty) data['Full Name'] = coreFields.fullName;
+    if (coreFields.firstName != null && coreFields.firstName!.isNotEmpty) data['First Name'] = coreFields.firstName;
+    if (coreFields.lastName != null && coreFields.lastName!.isNotEmpty) data['Last Name'] = coreFields.lastName;
+    if (coreFields.company != null && coreFields.company!.isNotEmpty) data['Company'] = coreFields.company;
+    if (coreFields.designation != null && coreFields.designation!.isNotEmpty) data['Designation'] = coreFields.designation;
+    if (coreFields.phonePrimary != null && coreFields.phonePrimary!.isNotEmpty) data['Phone (Primary)'] = coreFields.phonePrimary;
+    if (coreFields.phoneSecondary != null && coreFields.phoneSecondary!.isNotEmpty) data['Phone (Secondary)'] = coreFields.phoneSecondary;
+    if (coreFields.phoneWhatsapp != null && coreFields.phoneWhatsapp!.isNotEmpty) data['WhatsApp'] = coreFields.phoneWhatsapp;
+    if (coreFields.emailPrimary != null && coreFields.emailPrimary!.isNotEmpty) data['Email (Primary)'] = coreFields.emailPrimary;
+    if (coreFields.emailSecondary != null && coreFields.emailSecondary!.isNotEmpty) data['Email (Secondary)'] = coreFields.emailSecondary;
+    if (coreFields.website != null && coreFields.website!.isNotEmpty) data['Website'] = coreFields.website;
+    if (coreFields.addressLine1 != null && coreFields.addressLine1!.isNotEmpty) data['Address Line 1'] = coreFields.addressLine1;
+    if (coreFields.addressLine2 != null && coreFields.addressLine2!.isNotEmpty) data['Address Line 2'] = coreFields.addressLine2;
+    if (coreFields.city != null && coreFields.city!.isNotEmpty) data['City'] = coreFields.city;
+    if (coreFields.state != null && coreFields.state!.isNotEmpty) data['State'] = coreFields.state;
+    if (coreFields.pincode != null && coreFields.pincode!.isNotEmpty) data['Pincode'] = coreFields.pincode;
+    if (coreFields.country != null && coreFields.country!.isNotEmpty) data['Country'] = coreFields.country;
+    if (coreFields.linkedinUrl != null && coreFields.linkedinUrl!.isNotEmpty) data['LinkedIn'] = coreFields.linkedinUrl;
+    if (coreFields.twitterHandle != null && coreFields.twitterHandle!.isNotEmpty) data['Twitter'] = coreFields.twitterHandle;
+    if (coreFields.aadhaarNumber != null && coreFields.aadhaarNumber!.isNotEmpty) data['Aadhaar Number'] = coreFields.aadhaarNumber;
+    if (coreFields.panNumber != null && coreFields.panNumber!.isNotEmpty) data['PAN Number'] = coreFields.panNumber;
+    if (coreFields.dob != null && coreFields.dob!.isNotEmpty) data['Date of Birth'] = coreFields.dob;
+    if (coreFields.gender != null && coreFields.gender!.isNotEmpty) data['Gender'] = coreFields.gender;
+    if (coreFields.notes != null && coreFields.notes!.isNotEmpty) data['Notes'] = coreFields.notes;
   }
   
   for (var df in dynFields) {
